f55e66e8f0b7bce59f6c53d426fec76caac1a5ff
vildozo <vildozo@outlook.com>
2017-03-03 15:51:05 -0400

    adding feature and step definitions

---
+++ src/stepDefinition/stepDefinitions.java
@@ -0,0 +1,40 @@
+package stepDefinition;
+
+public class stepDefinitions {
+	@Given("^User is on Home Page$")
+	public void user_is_on_Home_Page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^User Navigate to LogIn Page$")
+	public void user_Navigate_to_LogIn_Page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^User enters UserName and Password$")
+	public void user_enters_UserName_and_Password() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Message displayed Login Successfully$")
+	public void message_displayed_Login_Successfully() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^User LogOut from the Application$")
+	public void user_LogOut_from_the_Application() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Message displayed LogOut Successfully$")
+	public void message_displayed_LogOut_Successfully() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+}
